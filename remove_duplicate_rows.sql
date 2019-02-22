457d2db5836a62f9467c14f97ba8d93c4de5435e
Bhaskar <bkotu6717@gmail.com>
2019-02-22 12:46:26 +0530

    Update remove_duplicate_rows.sql

--- remove_duplicate_rows.sql
+++ remove_duplicate_rows.sql
@@ -29,6 +29,7 @@ DELETE FROM basket a
 USING basket b
 WHERE a.id < b.id AND a.fruit = b.fruit;
 
+-- LEVEL 2
 CREATE TABLE employee(
  id SERIAL PRIMARY KEY,
  first_name VARCHAR(50) NOT NULL,
@@ -49,11 +50,33 @@ INSERT INTO employee(first_name, last_name, designation, department) VALUES('Pau
 SELECT first_name, last_name, COUNT(*) FROM employee group by first_name, last_name 
 HAVING COUNT(*) > 1;
 
--- DISTINCT ROWS ALONG WITH ID 
-SELECT DISTINCT ON(first_name, last_name) *FROM employee;
+SELECT * FROM employee;
 
+-- DISTINCT ROWS ALONG WITH ID 
+SELECT DISTINCT ON(first_name, last_name) * FROM employee;
+
+-- DELETES ROWS WHICH ARE DUPLICATES
 DELETE FROM employee WHERE id NOT IN (
 SELECT id FROM(
 	SELECT DISTINCT ON (f.first_name, f.last_name) * FROM employee f
 ) e);
+
+-- COMBINE AND SLECT DUPLICATE ROWS INTO SINGLE ROW
+SELECT
+  first_name,
+  last_name,
+  string_agg(designation, ' / ') AS designations,
+  string_agg(department, ' / ') AS departments
+FROM employee
+GROUP BY
+  first_name,
+  last_name;
+  
+ 
+ first_name | last_name |             designations              |              departments              
+------------+-----------+---------------------------------------+---------------------------------------
+ Olivier    | Le Blanc  | PDG / PDG                             | RTM / RTM
+ Paul       | Jones     | Developer / Developer / Project Chief | RTM/FMP / RTM/FMP / RTM/BSO
+ Maria      | Green     | Intern / Intern / RH / DRH            | STP/RMP / STP/RMP / STP/RMP / STP/RMP
+(3 rows)
 
